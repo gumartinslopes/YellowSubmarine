566070a11f3f8d8bc2bad437b4f1a07eb3102854
Bárbara Carmo <babi.carmo@outlook.com>
2022-06-01 12:01:42 -0300

    Menu e animação

--- Assets/Scripts/scene manipulation/ChangeScene.cs
+++ Assets/Scripts/scene manipulation/ChangeScene.cs
@@ -6,9 +6,17 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public string sceneName;
+    public Animator anim;
 
     public void ChangeS() {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene() {
+        anim.SetBool("call", true);
+        yield return new WaitForSeconds(2.0f);
         SceneManager.LoadScene(sceneName);
+        anim.SetBool("call", false);
     }
 
     public void Sair() {
